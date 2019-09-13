217cf4eb7372d755d500a8a6f5d1ccd2970f18f4
jimmyCA2015 <xianglinbusiness@hotmail.ca>
2019-09-13 01:03:30 -0400

    testJimmy

--- src/test/java/webdriverMaven/LoginTest.java
+++ src/test/java/webdriverMaven/LoginTest.java
@@ -2,6 +2,7 @@ package webdriverMaven;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -13,12 +14,13 @@ public class LoginTest {
 	public static WebDriver driver;;
 	@BeforeSuite
 	public void setup() {
-		driver=new FirefoxDriver();
+		driver=new ChromeDriver();
 	}
 	@Test
 	public void doLogin() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jimmy\\Desktop\\chromedriver.exe"); 
 		driver.get("http://gmail.com");
-		driver.findElement(By.id("identifierId")).sendKeys("traiaaner@way2automation.com");
+		driver.findElement(By.id("identifierId")).sendKeys("traiaane22r@way2automation.com");
 		driver.findElement(By.id("identifierNext")).click();
 	}
 	@AfterSuite 
